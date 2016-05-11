d8c784a67c388fabbad35ad9dd3203e206bad46c
Thorsten <thorsten@potpiejimmy.de>
2016-05-11 17:19:37 +0200

    Initial import

---
+++ app/app.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component } from '@angular/core';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
+
+@Component({
+  selector: 'app',
+  providers: [HTTP_PROVIDERS],
+  template: `
+    <h1>Result:</h1>
+    <h3>{{dynresult}}</h3>
+  `
+})
+export class AppComponent implements OnInit {
+    dynresult = 'Loading details...';
+
+    constructor(private http: Http) { }
+
+    ngOnInit() {
+        this.loadDetails();
+    }
+
+    loadDetails() {
+        this.http.get('/rest/dummydata.json')
+          .subscribe(
+            res => { this.dynresult = res.json()['mail']},
+            err => console.error(err),
+            () => console.log('done')
+        );
+    }
+}
